24b2da7913e1fc6fd3fa10e80114e8efc4cbd09d
LUCAaaa01 <s287146@studenti.polito.it>
2025-04-30 10:59:14 +0200

    Update PolygonalMesh.hpp

--- Exercise2/src/PolygonalMesh.hpp
+++ Exercise2/src/PolygonalMesh.hpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <vector>
+#include <map> //
 #include "Eigen/Eigen"
 
 using namespace std;
@@ -15,24 +16,24 @@ struct PolygonalMesh
     unsigned int NumCell1Ds;
     unsigned int NumCell2Ds;
 
-    // 0D: Id e Marker
     vector<unsigned int> Cell0DsId;
     vector<unsigned int> Cell0DsMarker;  
 
-    // 1D: Id, Marker e indici degli estremi
     vector<unsigned int> Cell1DsId;
     vector<unsigned int> Cell1DsMarker;    
     vector<unsigned int> Cell1DsOrigin;    
-    vector<unsigned int> Cell1DsEnd;       
+    vector<unsigned int> Cell1DsEnd;
+
     vector<unsigned int> Cell2DsId;
     vector<unsigned int> Cell2DsMarker;
     vector<vector<unsigned int>> Cell2DsVertices;
     vector<vector<unsigned int>> Cell2DsEdges;
 
-    
+    map<unsigned int, unsigned int> Cell0DIdToIndex; // <--- AGGIUNTO
+
     MatrixXd Cell0DsCoordinates;
     MatrixXd Cell1DsCoordinates;
     MatrixXd Cell2DsCoordinates;
 };
 
-} 
+}
